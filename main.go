0745c2bba2eabc317f930c4a763c53f7c67ec45c
moravi <mohan_ravi@bmc.com>
2021-05-30 21:39:25 +0530

    changes

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	coreV1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+
+	//"reflect"
 
 	"log"
 )
@@ -18,7 +21,7 @@ func main() {
 	//	kubeconfig = flag.String("kubeconfig", "", "complete path for kubeconfig")
 	//}
 	//flag.Parse()
-
+	//
 	//conf, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	conf, err := rest.InClusterConfig()
 	if err != nil {
@@ -34,11 +37,35 @@ func main() {
 	if err != nil {
 		log.Fatalf("couldnt able to talk to cluster and the error is: %v", err)
 	}
+	var api = client.CoreV1().Namespaces()
+	//endpoints, err := api.List(metav1.ListOptions{})
 	//fmt.Println(reflect.TypeOf(namespace))
 	//println(namespace.Items)
 	for _, s := range namespace.Items {
 		//fmt.Println("I is", i, "S is", s)
 		fmt.Println(s.Name)
+	}
+
+	namespaceVersion := namespace.ListMeta.ResourceVersion
+	//fmt.Println(reflect.TypeOf(namespaceVersion))
+	//fmt.Println("namespaceresource version is", namespaceVersion)
+
+	watcher, err := api.Watch(context.TODO(), metav1.ListOptions{ResourceVersion: namespaceVersion})
+
+	if err != nil {
+		panic(err.Error())
+	}
+	ch := watcher.ResultChan()
+	for {
+		event := <-ch
+		endpoints, ok := event.Object.(*coreV1.Namespace)
+		if !ok {
+			panic("Could not cast to Endpoint")
+		}
+		fmt.Printf("%v\n", endpoints.ObjectMeta.Name)
+		for _, endpoint := range endpoints.Namespace {
+			fmt.Printf("%v\n", endpoint)
+		}
 	}
 
 }
